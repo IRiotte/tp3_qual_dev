1185282b2e75938e80a95ba77a477e68946a172c
IRiotte <ilane.riotte@etu.univ-orleans.fr>
2025-03-06 09:48:31 +0100

    TP

--- src/Box.java
+++ src/Box.java
@@ -3,17 +3,83 @@ import java.util.ArrayList;
 
 class Thing{
     String name;
+    int volume;
+
     public Thing(String name){
         this.name = name;
+    }
+
+    public Thing(int volume){
+        this.volume = volume;
+    }
+
+    public int volume(){
+        return this.volume;
     }
 }
 
 class Box{
 
-    ArrayList<Thing> contents = new ArrayList<Thing>();
+    private ArrayList<Thing> contents = new ArrayList<Thing>();
+    private boolean open = true;
+    private int capacity = -1;
 
     public void add(Thing truc) {
         this.contents.add(truc);
     }
+
+    public boolean estDans(Thing truc){
+        return this.contents.contains(truc);
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void close() {
+        open = false;
+    }
+
+    public void open() {
+        open = true;
+    }
+
+    public void remove(Thing truc) {
+        this.contents.remove(truc);
+        //lever une exception si le truc n'est pas dans la boite
+    }
+
+
+    public String actionLook() {
+        if (open) {
+            String res = "la boite contient: ";
+            for (Thing truc : this.contents) {
+                res += truc.name + ", ";
+            }
+            return res;
+            }
+        return "la boite est fermee";
+    }
+
+    public int capacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean hasRoomFor(Thing truc) {
+        if (capacity == -1) {
+            return true;
+        }
+        int totalVolume = 0;
+        for (Thing t : this.contents) {
+            totalVolume += t.volume();
+        }
+        return totalVolume + truc.volume() <= capacity;
+    }
+
+
 
 }
--- src/TestsBoxes.java
+++ src/TestsBoxes.java
@@ -15,4 +15,74 @@ import static org.junit.Assert.assertEquals;
         b.add(new Thing("truc1"));
         b.add(new Thing("truc2"));
     }
+
+    // on veut pouvoir vérifier si un truc est dans la boite
+    @Test
+    public void testBoxContains(){
+        Box b = new Box();
+        Thing t = new Thing("truc1");
+        b.add(t);
+        assertEquals(true, b.estDans(t));
+    }
+
+    // on veut pouvoir ouvrir et fermer la boite
+    @Test
+    public void testBoxOpenClose(){
+        Box b = new Box();
+        assertEquals(true, b.isOpen());
+        b.close();
+        assertEquals(false, b.isOpen());
+        b.open();
+        assertEquals(true, b.isOpen());
+    }
+
+    // on veut pouvoir enlever un truc de la boite
+    @Test
+    public void testBoxRemove(){
+        Box b = new Box();
+        Thing t = new Thing("truc1");
+        b.add(t);
+        b.remove(t);
+        assertEquals(false, b.estDans(t));
+    }
+
+    // on veut pouvoir regarder dans la boite
+    @Test
+    public void testBoxLook(){
+        Box b = new Box();
+        Thing t = new Thing("truc1");
+        b.add(t);
+        assertEquals("la boite contient: truc1, ", b.actionLook());
+        b.close();
+        assertEquals("la boite est fermee", b.actionLook());
+    }
+
+
+    @Test
+    public void testThingBoxVolumeCapacity(){
+        Box b = new Box();
+        Thing t = new Thing(3);
+        assertEquals(3, t.volume());
+        assertEquals(-1, b.capacity());
+        b.setCapacity(5);
+        assertEquals(5, b.capacity());
+    }
+
+
+    @Test
+    public void testBoxHasRoomFor() {
+        Box b = new Box();
+        b.setCapacity(10);
+        Thing t1 = new Thing(3);
+        Thing t2 = new Thing(7);
+        Thing t3 = new Thing(1);
+        
+        assertEquals(true, b.hasRoomFor(t1));
+        b.add(t1);
+        assertEquals(true, b.hasRoomFor(t2));
+        b.add(t2);
+        assertEquals(false, b.hasRoomFor(t3));
+    }
+
+
 };
